d502b96ede7092b629c509ae1a81e469b700fe7d
cha <chyb627@naver.com>
2024-10-15 17:51:04 +0900

    first commit

---
+++ src/main/java/com/youngbin/JavaBatchApplication.java
@@ -0,0 +1,13 @@
+package com.youngbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youngbin/JavaBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
